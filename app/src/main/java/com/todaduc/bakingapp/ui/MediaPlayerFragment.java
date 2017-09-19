a822d093a162db945cccf82aa4fa4eab97be6a15
todaduc <todaduc@gmail.com>
2017-09-19 15:39:07 -0400

    initialized the player

--- app/src/main/java/com/todaduc/bakingapp/ui/MediaPlayerFragment.java
+++ app/src/main/java/com/todaduc/bakingapp/ui/MediaPlayerFragment.java
@@ -1,11 +1,26 @@
 package com.todaduc.bakingapp.ui;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.VideoView;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.todaduc.bakingapp.R;
 
 import butterknife.BindView;
@@ -18,7 +33,9 @@ import butterknife.ButterKnife;
 public class MediaPlayerFragment extends Fragment {
 
     @BindView(R.id.media_player_view)
-    VideoView videoView;
+    SimpleExoPlayerView mPlayerView;
+    private SimpleExoPlayer mExoPlayer;
+
 
     public MediaPlayerFragment() {
     }
@@ -28,13 +45,43 @@ public class MediaPlayerFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_media_player, container, false);
         ButterKnife.bind(this,rootView);
 
+
         String videoUrl = null;
 
         if( getArguments()!= null){
             videoUrl = getArguments().getString("Video");
+            Log.i("videoUrl ", videoUrl);
+            initializePlayer(Uri.parse(videoUrl));
         }
 
         return rootView;
     }
 
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(getContext(), "BakingApp");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
 }
