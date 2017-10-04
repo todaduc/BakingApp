3d3f8c8ff93f36d832f29fdf4434c20c49156d23
todaduc <todaduc@gmail.com>
2017-10-04 15:59:15 -0400

    tested mainactivity

--- app/src/androidTest/java/com/todaduc/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/com/todaduc/bakingapp/MainActivityTest.java
@@ -8,6 +8,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+
 /**
  * Created by Themener on 10/3/17.
  */
@@ -15,6 +23,7 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
+    public static final String RECIPE_NAME = "Brownies";
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
@@ -23,6 +32,11 @@ public class MainActivityTest {
     @Test
     public void clickGridViewItem_OpensRecipeDetailActivity(){
 
+        onData(anything()).inAdapterView(withId(R.id.recipe_grid_view)).atPosition(1).perform(click());
+        onView(withId(R.id.recipe_ingredients_container));
+        onView(withId(R.id.recipe_detail_container));
 
+        // Checks that the OrderActivity opens with the correct tea name displayed
+       onView(withId(R.string.activity_label)).check(matches(withText(RECIPE_NAME)));
     }
 }
