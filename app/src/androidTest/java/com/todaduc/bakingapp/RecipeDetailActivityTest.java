37fb09054d2a9c8b2013df5f3d3dd77aa397035d
todaduc <todaduc@gmail.com>
2017-11-15 16:43:35 -0500

    updated the test

--- app/src/androidTest/java/com/todaduc/bakingapp/RecipeDetailActivityTest.java
+++ app/src/androidTest/java/com/todaduc/bakingapp/RecipeDetailActivityTest.java
@@ -7,12 +7,16 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import com.todaduc.bakingapp.ui.activities.RecipeDetailActivity;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
 
 
 /**
@@ -29,8 +33,14 @@ public class RecipeDetailActivityTest {
      */
     @Test
     public void clickGridViewItem_OpensStepsDetailActivity(){
-        onView(ViewMatchers.withId(R.id.tv_recipe_serving))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(3, ViewActions.click()));
-        onView(withId(R.id.recipe_detail_container));
+
+       //onView(ViewMatchers.withId(R.id.tv_recipe_serving));
+       onView(ViewMatchers.withId(R.id.recipe_ingredients_container));
+       onView(withId(R.id.recipe_detail_container));
+       onView(withId(R.id.listStepView)).perform(RecyclerViewActions.actionOnItemAtPosition(3, ViewActions.click()));
+      // onData(anything()).inAdapterView(withId(R.id.listStepView)).atPosition(1).perform(click());
+        onView(withId(R.id.video_player_container));
+             // .perform(RecyclerViewActions.actionOnItemAtPosition(3, ViewActions.click()));
+
     }
 }
